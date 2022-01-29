d6bf024c8d03ac2895566f07bff008cc0532384c
rubencito <ruben.santillan0902@gmail.com>
2022-01-29 15:10:51 -0500

    edit-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,14 +30,12 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user.skip_password_validation = true
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
