f1c5aeac7dc34856685f0581b83d90ae65645ce4
eduardo4394 <eduardo_sh@outlook.com>
2022-01-30 01:00:41 -0500

    added create and destroy for controllers

---
+++ app/controllers/involved_companies_controller.rb
@@ -0,0 +1,27 @@
+class InvolvedCompaniesController < ApplicationController
+    before_action :set_game, only: %i[create destroy]
+    
+    
+  def create           
+    @game = Game.find(params[ :game_id ])
+    @company = Company.find(params[:involved_company][:id])
+    if params[:commit] == "Add Publisher"
+      @game.involved_companies.create(game: @game,company: @company, publisher: true, developer: false)
+    elsif params[:commit] == "Add Developer"
+      @game.involved_companies.create(game: @game,company: @company, developer: true, publisher: false)
+    end
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game = Game.find(params[:game_id])
+    @involved_company = @game.involved_companies.find(params[:id])
+    @game.involved_companies.destroy(@involved_company)
+    redirect_to root_path, notice: "Company was successfully destroyed."
+  end
+
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+  end
